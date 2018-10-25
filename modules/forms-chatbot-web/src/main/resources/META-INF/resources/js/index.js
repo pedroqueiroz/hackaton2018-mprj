b7baa3e75724f1bde2096d00b4f57072d0aa70e7
Adriano <adriano.interaminense@gmail.com>
2018-10-25 17:51:42 -0300

    Fetch in progress

--- modules/forms-chatbot-web/src/main/resources/META-INF/resources/js/index.js
+++ modules/forms-chatbot-web/src/main/resources/META-INF/resources/js/index.js
@@ -2,7 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ChatBot from 'react-simple-chatbot';
 
+const {getFormEntriesByUserURL, getFormDefinition, saveFormEntry} = window.chatBotConstants;
+
+console.log(getFormEntriesByUserURL, getFormDefinition, saveFormEntry);
+
 class App extends React.Component {
+	componentWillMount() {
+		this.getFormEntriesByUserURL();
+		this.getFormDefinition();
+		this.saveFormEntry();
+	}
+
+	getFormEntriesByUserURL() {
+		fetch(getFormEntriesByUserURL).then(response => {
+			response.json().then(() => {
+				console.log(response, 'getFormEntriesByUserURL');
+			})
+		}).catch(error => console.log(error));
+	}
+
+	getFormDefinition() {
+		fetch(getFormDefinition).then(response => {
+			response.json().then(() => {
+				console.log(response, 'getFormDefinition');
+			})
+		}).catch(error => console.log(error));
+	}
+
+	saveFormEntry() {
+		fetch(saveFormEntry).then(response => {
+			response.json().then(() => {
+				console.log(response, 'saveFormEntry');
+			})
+		}).catch(error => console.log(error));
+	}
+
 	render() {
 		return (
 			<div>
