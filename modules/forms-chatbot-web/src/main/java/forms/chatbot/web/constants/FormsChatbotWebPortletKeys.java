eec2371397c7de27a8a014952049ef3e42e63e39
Pedro Queiroz <pedro.queiroz@liferay.com>
2018-10-25 14:08:48 -0300

    Add a npm react module boilerplate

---
+++ modules/forms-chatbot-web/src/main/java/forms/chatbot/web/constants/FormsChatbotWebPortletKeys.java
@@ -0,0 +1,10 @@
+package forms.chatbot.web.constants;
+
+/**
+ * @author pedroqueiroz94
+ */
+public class FormsChatbotWebPortletKeys {
+
+	public static final String FormsChatbotWeb = "formschatbotweb";
+
+}
---
+++ modules/forms-chatbot-web/src/main/java/forms/chatbot/web/constants/FormsChatbotWebWebKeys.java
@@ -0,0 +1,10 @@
+package forms.chatbot.web.constants;
+
+/**
+ * @author pedroqueiroz94
+ */
+public class FormsChatbotWebWebKeys {
+
+	public static final String BOOTSTRAP_REQUIRE = "BOOTSTRAP_REQUIRE";
+
+}
